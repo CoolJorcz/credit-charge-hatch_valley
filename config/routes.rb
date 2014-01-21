36307c12cb87fd1e45ee277f98b97e7894cd0403
Andrew Jorczak <andrew.jorczak@gmail.com>
2014-01-21 15:59:51 -0600

    Updated README and root index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CreditChargeHatchValley::Application.routes.draw do
 
   resources :charges
+
+  root to: "charges#index"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
