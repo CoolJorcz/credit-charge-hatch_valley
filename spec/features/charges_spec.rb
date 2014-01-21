460991fe206f5e70461ed980a89ccb87aec5d78f
Andrew Jorczak <andrew.jorczak@gmail.com>
2014-01-21 15:36:22 -0600

    Passing tests for verification of data

--- spec/features/charges_spec.rb
+++ spec/features/charges_spec.rb
@@ -9,14 +9,6 @@ describe "Charges Index" do
 	before(:each) do
 
 		visit "/charges"
-
-		assert(:charge) { Charge.create!(Charge.create(created: rand_time(885413447.0, Time.now),
-								 paid: false,
-								 amount: (rand(99999)+ 1),
-								 currency: 'usd',
-								 refunded: true,
-								 dispute_date: Time.now,
-								 customer_id: 1))}
 	end
 
 	it "should have three lists" do
@@ -26,14 +18,15 @@ describe "Charges Index" do
 	end
 
 	it "should have 10 successful charges" do
-		page.
+		page.should have_css(".successful li", :count => 10 )
 	end
 
 	it "should have 5 failed charges" do
-
+		page.should have_css(".failed li", :count => 5 )
 	end
 
 	it "should have failed charges in disputed charges" do
-
+		@charge = Charge.where(paid: false, refunded: true).first
+		page.should have_content("#{@charge[:name]}")
 	end
 end
