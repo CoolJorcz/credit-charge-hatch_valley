7473f2a5288cdbfd26c8977bde7965be845a814e
Andrew Jorczak <andrew.jorczak@gmail.com>
2014-01-21 14:37:10 -0600

    Created scopes for failed, successful, and disputed charges

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,4 +1,7 @@
 class Charge < ActiveRecord::Base
 	belongs_to :customer
 	validates_presence_of :amount, :currency, :customer_id
+	scope :failed, -> { where(paid: false) }
+	scope :successful, -> { where(paid: true) }
+	scope :disputed, -> { where('dispute_date > 0 AND refunded = true') }
 end
