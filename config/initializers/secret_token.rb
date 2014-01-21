95565596e75a89ef39e642c1e0c8a98aa19ccdea
Andrew Jorczak <andrew.jorczak@gmail.com>
2014-01-21 13:49:38 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreditChargeHatchValley::Application.config.secret_key_base = '1043f4766f35ae7a8b0e4786027a55c8b8644f6cadc04fda1a3f246598d686fe9a4bf244da006fbfa5d8ba6ad105eabdddec21769633476fd829bb135f543b77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CreditChargeHatchValley::Application.config.session_store :cookie_store, key: '_credit_charge_hatch_valley_session'
